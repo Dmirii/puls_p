caa74b6a61e94fe1df09d1863b08f54065378512
Dima A <dimaa@dimaa.ru>
2020-05-26 14:35:32 +0300

    +
    +

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 console.log('my js conected');
 
 $(document).ready(function(){
+
+    // это карусель
     $('.carusel__inner').slick({
         speed:1200,
         //adaptiveHeight:true,
@@ -18,6 +20,13 @@ $(document).ready(function(){
               }
         ]
 
+    });
+
+    // это табы
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
     
 });
