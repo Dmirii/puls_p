a3f64f609c74ff9cc1b2fb6c7b2283d8dc01dafc
Dima A <dimaa@dimaa.ru>
2020-05-27 11:12:55 +0300

    +
    +add api key

--- js/script.js
+++ js/script.js
@@ -28,5 +28,25 @@ $(document).ready(function(){
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
+
+
+
+    function toggleSlide(classItem){
+       
+         $(classItem).each(function(i) {
+          $(this).on('click', function(e){
+            e.preventDefault();
+            $('.catalog-item__chg-content').eq(i).toggleClass('catalog-item__chg-content_active');
+    
+            $('.catalog-item__chg-list').eq(i).toggleClass('catalog-item__chg-list_active');
+    
+          })
+      })
+
+    };
+
+    toggleSlide('.catalog-item__back');
+    toggleSlide('.catalog-item__link');
+   
     
 });
